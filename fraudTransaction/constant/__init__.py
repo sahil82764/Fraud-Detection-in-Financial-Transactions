7379cd5b0936c6167ac567a2691b95d2e2858b0c
sahil82764 <sahil82764@gmail.com>
2024-04-09 15:56:05 -0400

    new files added

--- fraudTransaction/constant/__init__.py
+++ fraudTransaction/constant/__init__.py
@@ -16,8 +16,6 @@ CONFIG_FILE_PATH = os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 CURRENT_TIME_STAMP = get_current_time_stamp()
 
 
-
-
 # Training pipeline related variable
 TRAINING_PIPELINE_CONFIG_KEY = "training_pipeline_config"
 TRAINING_PIPELINE_ARTIFACT_DIR_KEY = "artifact_dir"
